05764b83ccd19f6258684ea03e52cb8c296e0b32
José Luis Olivares <joselukeolivares@gmail.com>
2018-08-27 09:29:05 -0600

    Primer commir con efecto de scratcher.js
    Se agrego el efecto Scratcher con el script Scratcher y manejo de promesas de manera nativa con JS.

---
+++ Script/Scratcher.js
@@ -0,0 +1,67 @@
+console.log("SCRATCHER ON");
+
+var div=document.createElement("div");
+var width=window.innerWidth;
+var height=window.innerHeight;
+    div.setAttribute("class","Scratcher_div");
+    div.style.width=width+"px";
+    div.style.display="none";
+    div.style.height=height+"px";
+
+    console.log("height:"+height+"pxXD");
+
+    document.body.appendChild(div);
+
+
+
+
+
+var scratching=function (num){
+    div.style.display="block";
+    console.log("Scratching effect "+num)
+
+    return  new Promise(function(resolve,reject){
+
+            var frame=0;
+
+            var interval=setInterval(function(){
+                  frame++;
+                  if(frame>18){
+                     clearInterval(interval);
+                     div.style.display="none";
+                     resolve("SCRATCHER OK: "+num);
+                  }
+
+                  div.style.backgroundPosition=(num==1?0:100)+"%"+(frame*-100)+"%";
+                  console.log(frame);
+
+                },50);
+
+        });
+
+
+};
+
+ var print=function(status){
+
+    console.log(status+" print");
+    //app.stage.removeChild(anim);
+    //app.stage.removeChild(shop);
+
+    while(app.stage.children[0]) { app.stage.removeChild(app.stage.children[0]); }
+    debugger;
+
+};
+
+  var secondS=function(cadena){
+    return function(){
+
+      return scratching(cadena);
+    }
+  }
+/*
+    scratching(1)
+      .then(print)
+      .then(secondS(2))
+      .then(print);
+*/
